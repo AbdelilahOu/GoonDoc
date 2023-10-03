662043bedc37ad7eeac1e9f33c7ca7652026a9b7
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-10-03 10:04:49 +0000

    parse dynamic generated content

--- go.mod
+++ go.mod
@@ -5,5 +5,14 @@ go 1.20
 require (
 	github.com/PuerkitoBio/goquery v1.8.1 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/chromedp/cdproto v0.0.0-20230914224007-a15a36ccbc2e // indirect
+	github.com/chromedp/chromedp v0.9.2 // indirect
+	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.2.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,31 +1,29 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/chromedp/cdproto/dom"
+	"github.com/chromedp/chromedp"
 )
 
 func main() {
+	// apparently the content isnt generated on the server
+	// its dynamically generated so we have to use other packages
+	// to process the page with javascript
 	start("https://www.youtube.com/")
 }
 
 func start(URL string) {
-	// request the page
-	res, err := http.Get(URL)
-	// check for errors
-	if err != nil {
-		fmt.Println("error getting the page")
-	}
-	// close res process is done
-	defer res.Body.Close()
-	// check if we got a valid res
-	if res.StatusCode != 200 {
-		fmt.Println("status code error")
-	}
 	// Load the HTML document
-	doc, err := goquery.NewDocumentFromReader(res.Body)
+	doc, err := ParseWebApp(URL)
+	if err != nil {
+		fmt.Println("error parsing the webapp")
+	}
+	// select all vedio cards
 	if err != nil {
 		fmt.Println("error reading the page")
 	}
@@ -33,10 +31,45 @@ func start(URL string) {
 	vedioCards := doc.Find("ytd-rich-grid-row >  #contents > ytd-rich-item-renderer > #content > ytd-rich-grid-media > #dismissible")
 	//
 	vedioCards.Each(func(i int, card *goquery.Selection) {
+		fmt.Println(i)
 		// get the vedio image and the title and the channel name also the time it was published
-		cardImageUrl, exists := card.Find("img").Attr("src")
+		cardImages := card.Find("img")
+		imageSrc, exists := cardImages.First().Attr("src")
 		if exists {
-			fmt.Println("image src :", cardImageUrl)
+			fmt.Println("image src :", imageSrc)
 		}
 	})
+}
+
+func ParseWebApp(url string) (*goquery.Document, error) {
+	// where to store generated html
+	var outterHTML string
+	// create ctx
+	ctx, cancel := chromedp.NewContext(context.Background())
+	// cancel whene we done
+	defer cancel()
+	//
+	if err := chromedp.Run(ctx, chromedp.Tasks{
+		chromedp.Navigate(url),
+		// js rendering happens asynchronously and this call seems to be enough to account for that
+		chromedp.WaitReady(":root"),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			node, err := dom.GetDocument().Do(ctx)
+			if err != nil {
+				return err
+			}
+			// get html
+			outterHTML, err = dom.GetOuterHTML().WithNodeID(node.NodeID).Do(ctx)
+			return err
+		}),
+	}); err != nil {
+		return nil, fmt.Errorf("ParseWebApp(): ActionFunc(): %w", err)
+	}
+	// parse html
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(outterHTML))
+	if err != nil {
+		return nil, fmt.Errorf("ParseWebApp(): goquery.NewDocumentFromReader(): %w", err)
+	}
+
+	return doc, nil
 }
